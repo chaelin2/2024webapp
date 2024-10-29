b3844eba27e2a5e8dd6c103705d010540f32eb39
chaelin2 <gcl693427@gmail.com>
2024-10-29 17:37:13 +0900

    LOGIN

--- 04react/2024login-main/src/pages/AboutComp.jsx
+++ 04react/2024login-main/src/pages/AboutComp.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import Layout from "../layout/Layout";
+import useCustomLogin from "../hooks/useCustomLogin";
 
 function AboutComp() {
+  const { isLogin, loginState, moveToLoginReturn } = useCustomLogin();
+
+  if (!isLogin) {
+    return moveToLoginReturn();
+  }
+
   return (
     <Layout>
       <div>AboutComp</div>
